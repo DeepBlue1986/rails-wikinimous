a040b7a026f6f01a44962dbf9572a1bb1fa3de65
Youssef Iraqui <youssef.iraqui@gmail.com>
2017-09-05 16:53:12 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+require "kramdown"
+10.times do
+  new_article = Article.create(title: Faker::Lorem.words, content: Faker::Lorem.words(3))
+  new_article.save
+end
+
+new_article = Kramdown::Article.create(title: "Ce title" , content: "Markdown **formatting** in my __code")
+new_article.save
